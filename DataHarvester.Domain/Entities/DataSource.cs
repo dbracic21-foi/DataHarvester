6a1dcc8dab80c420a145b98204450a484b4229d0
dbracic21@foi <dbracic21@student.foi.hr>
2025-07-23 22:27:46 +0400

    Set up initial database and create first migration

---
+++ DataHarvester.Domain/Entities/DataSource.cs
@@ -0,0 +1,9 @@
+﻿namespace DataHarvester.Domain.Entities;
+
+public class DataSource
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Type { get; set; } = null!;
+    public ICollection<DataItem> DataItems { get; set; } = new List<DataItem>();
+}
---
+++ DataHarvester.Domain/Entities/Favorite.cs
@@ -0,0 +1,12 @@
+﻿namespace DataHarvester.Domain.Entities;
+
+public class Favorite
+{
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+    
+    public Guid DataItemId { get; set; }
+    public DataItem DataItem { get; set; } = null!;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}

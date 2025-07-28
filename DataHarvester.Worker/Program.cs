30427937ae664799912931c00b681c204f65321e
dbracic21@foi <dbracic21@student.foi.hr>
2025-07-29 01:14:03 +0400

    Stop tracking appsettings files

--- DataHarvester.Worker/Program.cs
+++ DataHarvester.Worker/Program.cs
@@ -1,3 +1,5 @@
+using DataHarverster.Application.Services;
+using DataHarvester.Infrastructure.ExternalApis;
 using DataHarvester.Worker;
 using DataHarvester.Worker.Services;
 using Microsoft.Extensions.Configuration;
@@ -8,12 +10,12 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 Console.WriteLine("==== Loaded Configuration ====");
-foreach (var kvp in builder.Configuration.AsEnumerable())
-{
-    Console.WriteLine($"{kvp.Key} = {kvp.Value}");
-}
-Console.WriteLine("================================");
-builder.Services.AddHostedService<RabbitMqListenerService>();
 
+builder.Services.AddHostedService<RabbitMqListenerService>();
+builder.Services.AddScoped<WeatherApiService>();
+builder.Services.AddScoped<CryptoApiService>();
+builder.Services.AddScoped<ExternalApiServiceFactory>();
+//builder.Services.AddHttpClient(); 
+//builder.Services.AddHttpClient<IWeatherService, WeatherService>();
 var host = builder.Build();
 host.Run();
---
+++ DataHarvester.Worker/Services/WeatherService.cs
@@ -0,0 +1,22 @@
+﻿using DataHarvester.Worker.Models;
+
+namespace DataHarvester.Worker.Services;
+
+public class WeatherService : IWeatherService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+    private readonly ILogger<WeatherService> _logger;
+
+    public WeatherService(HttpClient httpClient, string apiKey, ILogger<WeatherService> logger)
+    {
+        _httpClient = httpClient;
+        _apiKey = apiKey;
+        _logger = logger;
+    }
+
+    public Task<WeatherResult?> FetchWeatherAsync(string city)
+    {
+        throw new NotImplementedException();
+    }
+}
